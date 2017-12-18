b55c566f87a7dbec94f9a675abe404969301a005
Daniel Pumayauli <danielpumayauli@gmail.com>
2017-12-18 00:08:53 -0500

    Añadido Lista Principal de Productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,13 @@
 namespace DanielPumayauli\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DanielPumayauli\Product;
 
 class ProductController extends Controller
 {
     public function index(){
-    	return 'index';
+    	$products = Product::orderBy('id', 'ASC')->paginate(10);
+    	// return view('products.index')->with(compact('products'));
+    	return view('products.index', compact('products'));
     }
 }
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layout')
+
+@section('content')
+
+<div class="col-sm-8">
+	<h2>Listado de Productos
+	<a href="{{route('products.create')}}" class="btn btn-primary pull-right">Agregar Nuevo</a>
+	</h2>
+	<table class="table table-hover table-striped">
+		<thead>
+			<tr>
+				<th width="20px">ID</th>
+				<th>Nombre del producto</th>
+				<th colspan="3">&nbsp;</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($products as $product)
+			<tr>
+				<td>{{$product->id}}</td>
+				<td>
+					<strong>
+						{{$product->name}}
+					</strong>
+					{{$product->short}}
+				</td>
+				<td>Ver</td>
+				<td>editar</td>
+				<td>borrar</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	{{$products->links()}}
+</div>
+<div class="col-sm-4">
+	hola mundo
+</div>
+
+@endsection
